d6caef5e4438778a581e4318596e0f8371805d95
Andrzej Maćkowski <vandmac@icloud.com>
2022-12-10 13:44:00 +0100

    change divs to buttons

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ console.log(guesWord);
 const keyboardArray = [];
 
 const keyboard = document.getElementById('keyboard');
+let letterKey;
+let letterToAdd;
 for (let i = 65; i < 91; i++) {
-  letterKey = document.createElement('div.letter');
+  letterKey = document.createElement('button');
   letterKey.classList.add('letter');
   letterToAdd = String.fromCharCode(i);
   keyboardArray.push(letterToAdd);
